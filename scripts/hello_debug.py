d4dbbdda9b403347e3867bc2a2bdc4f11baf8a7d
andrew.taibi <andrewvtaibi@gmail.com>
2026-04-13 12:34:55 -0600

    Removed unused/reduundant code and confirmed server is still functioning as expected.

--- scripts/hello_debug.py
+++
@@ -1,4 +0,0 @@
-print("HELLO from VS Code debugger")
-import sys, os
-print("python:", sys.executable)
-print("cwd:", os.getcwd())
--- scripts/test_env.py
+++
@@ -1,5 +0,0 @@
-import sys, platform, ssl
-print('OK: Python:', sys.version)
-print('OK: Executable:', sys.executable)
-print('OK: Platform:', platform.platform())
-print('OK: SSL:', ssl.OPENSSL_VERSION)
